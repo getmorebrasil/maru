f335047e471f3047b6581ca0572cc370623f18c4
Bruno Hass <bruno_hass@live.com>
2019-09-12 12:56:55 -0300

    Adds model to desc block
    This commit adds the model block to allow exporting API
    models to maru_swagger.

--- test/builder/plugins/description_test.exs
+++ test/builder/plugins/description_test.exs
@@ -40,4 +40,40 @@ defmodule Maru.Builder.DescriptionTest do
              ]
            } = DescTestWithBlock.d()
   end
+
+  test "description with block and model" do
+    defmodule DescTestWithBlock do
+      use Maru.Router
+
+      desc "desc test" do
+        detail("""
+        this is detail
+        """)
+
+        model "foo" do
+          field("bar", type: "integer", format: "int64")
+        end
+
+        responses do
+          status(200, desc: "ok")
+          status(500, desc: "error")
+        end
+      end
+
+      def d, do: @desc
+    end
+
+    assert %{
+             summary: "desc test",
+             detail: "this is detail\n",
+             model: %{
+                name: "foo",
+                fields: [%{name: "bar", type: "integer", format: "int64"}]
+             },
+             responses: [
+               %{code: 200, description: "ok"},
+               %{code: 500, description: "error"}
+             ]
+           } = DescTestWithBlock.d()
+  end
 end
